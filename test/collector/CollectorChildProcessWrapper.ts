3ce6cb72db78e00e1a6c07e1f4dde59e38719059
Bastian Krol <bastian.krol@dash0.com>
2024-05-14 11:23:01 +0200

    feat: minium version check

--- test/collector/CollectorChildProcessWrapper.ts
+++ test/collector/CollectorChildProcessWrapper.ts
@@ -19,6 +19,11 @@ export default class CollectorChildProcessWrapper extends ChildProcessWrapper {
     return stats.traces >= 1;
   }
 
+  async hasTelemetry() {
+    const stats = await collector().fetchStats();
+    return stats.traces >= 1 || stats.metrics >= 1 || stats.logs >= 1;
+  }
+
   async fetchTelemetry() {
     return <OpenTelemetryData>await collector().sendRequest({ command: 'telemetry' });
   }
