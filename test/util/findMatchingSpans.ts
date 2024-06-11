fe7d6ff4ed8369e9565ad527762174432af07e07
Bastian Krol <bastian.krol@dash0.com>
2024-06-11 10:34:45 +0200

    feat: add DASH0_DEBUG_PRINT_SPANS to print spans to stdout or file

--- test/util/findMatchingSpans.ts
+++ test/util/findMatchingSpans.ts
@@ -15,7 +15,7 @@ export interface MatchingSpansResult {
   lastError?: Error;
 }
 
-export default function findMatchingSpans(
+export function findMatchingSpans(
   traceDataItems: ExportTraceServiceRequest[],
   resourceExpectations: Expectation<Resource>[],
   spanExpectations: Expectation<Span>[],
@@ -60,6 +60,69 @@ export default function findMatchingSpans(
         });
       });
     });
+  });
+  if (lastError) {
+    return { matchingSpans, lastError };
+  } else {
+    return { matchingSpans };
+  }
+}
+
+export function findMatchingSpansInFileDump(
+  spans: any[],
+  resourceAttributeExpectations: Expectation<any>[],
+  spanExpectations: Expectation<any>[],
+  spanAttributeExpectations: Expectation<any>[],
+): MatchingSpansResult {
+  if (spans.length === 0) {
+    fail('No trace data has been provided.');
+  }
+  const matchingSpans: Span[] = [];
+
+  let lastError = null;
+  spans.forEach(span => {
+    if (resourceAttributeExpectations.length > 0) {
+      // verify that the resource attribtues match
+      const resource = span.resource;
+      if (!resource) {
+        // This resource span has no resource information, try the next span.
+        return;
+      }
+      const resourceAttributes = resource.attributes;
+      if (!resourceAttributes) {
+        // This resource span has no resource information, try the next span.
+        return;
+      }
+      try {
+        for (let i = 0; i < resourceAttributeExpectations.length; i++) {
+          resourceAttributeExpectations[i](resourceAttributes);
+        }
+      } catch (error) {
+        // This resource did not match, try the next resource span.
+        lastError = error;
+        return;
+      }
+    }
+
+    try {
+      for (let i = 0; i < spanExpectations.length; i++) {
+        spanExpectations[i](span);
+      }
+      if (spanAttributeExpectations.length > 0) {
+        const spanAttributes = span.attributes;
+        if (!spanAttributes) {
+          // This span has no attributes, try the next span.
+          return;
+        }
+        for (let i = 0; i < spanAttributeExpectations.length; i++) {
+          spanAttributeExpectations[i](spanAttributes);
+        }
+      }
+      matchingSpans.push(span);
+    } catch (error) {
+      // This span did not match, try the next span.
+      lastError = error;
+    }
   });
   if (lastError) {
     return { matchingSpans, lastError };
