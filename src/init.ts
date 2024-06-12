1c0a410dd0aa214f8487c694d1a567bb31352f20
Bastian Krol <bastian.krol@dash0.com>
2024-06-12 08:11:43 +0200

    feat: create bootstrap span on demand (DASH0_BOOTSTRAP_SPAN)

--- src/init.ts
+++ src/init.ts
@@ -1,6 +1,7 @@
 // SPDX-FileCopyrightText: Copyright 2024 Dash0 Inc.
 // SPDX-License-Identifier: Apache-2.0
 
+import { SpanKind, trace } from '@opentelemetry/api';
 import { getNodeAutoInstrumentations, getResourceDetectors } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
@@ -83,6 +84,16 @@ configuration.resourceDetectors = detectors;
 const sdk = new NodeSDK(configuration);
 
 sdk.start();
+
+if (process.env.DASH0_BOOTSTRAP_SPAN != null) {
+  const tracer = trace.getTracer('dash0-nodejs-distribution');
+  tracer //
+    .startSpan(process.env.DASH0_BOOTSTRAP_SPAN, {
+      root: true,
+      kind: SpanKind.INTERNAL,
+    })
+    .end();
+}
 
 if (process.env.DASH0_DEBUG) {
   console.log('Dash0 OpenTelemetry distribution for Node.js: NodeSDK started.');
--- test/util/expectMatchingSpan.ts
+++ test/util/expectMatchingSpan.ts
@@ -4,15 +4,15 @@
 import { ExportTraceServiceRequest } from '../collector/types/opentelemetry/proto/collector/trace/v1/trace_service';
 import { Resource } from '../collector/types/opentelemetry/proto/resource/v1/resource';
 import { Span } from '../collector/types/opentelemetry/proto/trace/v1/trace';
-import { Expectation, findMatchingSpans, findMatchingSpansInFileDump } from './findMatchingSpans';
+import { Expectation, findMatchingSpans, findMatchingSpansInFileDump, MatchingSpansResult } from './findMatchingSpans';
 
 export function expectMatchingSpan(
   traceDataItems: ExportTraceServiceRequest[],
   resourceExpectations: Expectation<Resource>[],
   spanExpectations: Expectation<Span>[],
 ): Span {
-  const { matchingSpans, lastError } = findMatchingSpans(traceDataItems, resourceExpectations, spanExpectations);
-  return processFindSpanResult(matchingSpans, lastError);
+  const matchResult = findMatchingSpans(traceDataItems, resourceExpectations, spanExpectations);
+  return processFindSpanResult(matchResult);
 }
 
 export function expectMatchingSpanInFileDump(
@@ -21,29 +21,33 @@ export function expectMatchingSpanInFileDump(
   spanExpectations: Expectation<any>[],
   spanAttributeExpectations: Expectation<any>[],
 ): Span {
-  const { matchingSpans, lastError } = findMatchingSpansInFileDump(
+  const matchResult = findMatchingSpansInFileDump(
     spans,
     resourceAttributeExpectations,
     spanExpectations,
     spanAttributeExpectations,
   );
-  return processFindSpanResult(matchingSpans, lastError);
+  return processFindSpanResult(matchResult);
 }
 
-function processFindSpanResult(matchingSpans: Span[], lastError: Error | undefined): Span {
-  if (matchingSpans.length === 1) {
-    return matchingSpans[0];
-  } else if (matchingSpans.length === 0) {
-    if (lastError) {
-      throw new Error(`No matching span found. Most recent failing expectation: ${lastError}`);
+function processFindSpanResult(matchResult: MatchingSpansResult): Span {
+  if (matchResult.matchingSpans) {
+    const matchingSpans = matchResult.matchingSpans;
+    if (matchingSpans.length === 1) {
+      return matchingSpans[0];
+    } else if (matchingSpans.length > 1) {
+      throw new Error(
+        `Expected exactly one matching span, found ${matchingSpans.length}.\nMatches:\n${JSON.stringify(matchingSpans, null, 2)}`,
+      );
     } else {
-      throw new Error('No matching span found.');
+      throw new Error('Unexpected error while processing matching spans.');
     }
-  } else if (matchingSpans.length > 1) {
+  } else if (matchResult.bestCandidate) {
+    const bestCandidate = matchResult.bestCandidate;
     throw new Error(
-      `Expected exactly one matching span, found ${matchingSpans.length}.\nMatches:\n${JSON.stringify(matchingSpans, null, 2)} `,
+      `No matching span has been found. The best candidate passed ${bestCandidate.passedChecks} and failed check ${bestCandidate.passedChecks + 1} with error ${bestCandidate.error}. This is the best candidate:\n${JSON.stringify(bestCandidate.spanLike, null, 2)}`,
     );
   } else {
-    throw new Error('Unexpected error while processing matching spans.');
+    throw new Error('No matching span has been found.');
   }
 }
