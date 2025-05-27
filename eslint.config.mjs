4edff84a0b3877a485c64c8e393175045e009f41
Bastian Krol <bastian.krol@dash0.com>
2025-05-27 14:08:59 +0200

    chore: temporarily disable eslint-plugin-header
    This plug-in is currently not compatible with eslint 9.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,6 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-import header from 'eslint-plugin-header';
 import mocha from 'eslint-plugin-mocha';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import unusedImports from 'eslint-plugin-unused-imports';
@@ -16,7 +15,9 @@ export default tseslint.config(
   tseslint.configs.recommended,
   {
     plugins: {
-      header,
+      // Temporarily disabled until eslint-plugin-header is compatible with eslint 9:
+      // https://github.com/Stuk/eslint-plugin-header/issues/59
+      // header,
       mocha,
       simpleImportSort,
       unusedImports,
@@ -28,18 +29,19 @@ export default tseslint.config(
     ],
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
-      'header/header': [
-        2,
-        'line',
-        [
-          {
-            pattern: ' SPDX-FileCopyrightText: Copyright \\d{4} Dash0 Inc\\.',
-            template: ` SPDX-FileCopyrightText: Copyright ${new Date().getFullYear()} Dash0 Inc.`,
-          },
-          ' SPDX-License-Identifier: Apache-2.0',
-        ],
-        2,
-      ],
+      // Temporarily disabled until eslint-plugin-header is compatible with eslint 9:
+      // https://github.com/Stuk/eslint-plugin-header/issues/59
+      // 'header/header': [
+      //   2,
+      //   'line',
+      //   [
+      //     {
+      //       pattern: ' SPDX-FileCopyrightText: Copyright \\d{4} Dash0 Inc\\.',
+      //       template: ` SPDX-FileCopyrightText: Copyright ${new Date().getFullYear()} Dash0 Inc.`,
+      //     },
+      //     ' SPDX-License-Identifier: Apache-2.0',
+      //   ],
+      // ],
       'mocha/no-exclusive-tests': 'error',
       'no-case-declarations': 'off',
       'simpleImportSort/exports': 'error',
